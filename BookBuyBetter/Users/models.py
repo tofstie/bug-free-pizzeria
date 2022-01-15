e9cf478ae766f1dbae15eb5bb3e4a8eb2f1ff92c
tofstie <60015373+tofstie@users.noreply.github.com>
2022-01-15 13:46:21 -0700

    Updated user model

--- BookBuyBetter/Users/models.py
+++ BookBuyBetter/Users/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
+from .managers import CustomUserManager
 # Create your models here.
+
+
+class User(AbstractUser):
+    username = models.CharField(max_length=50)
+    university = models.CharField(max_length=100, blank=False)
+    email = models.EmailField(_('email adress'), unique=True)
+    current_classes = models.JSONField()
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects= CustomUserManager()
+
+    def save(self, *args, **kwargs):
+        self.username = self.university.split('@')[0]
+        super().save(*args, **kwargs)
+        
+    def __str__(self):
+        return self.email
