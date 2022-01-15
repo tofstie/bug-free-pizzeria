a85543ac42f0810b3ce6efc893e7a818371f0e24
unknown <skousboL@ualberta.ca>
2022-01-15 16:10:14 -0700

    DB working now

--- BookBuyBetter/Users/models.py
+++ BookBuyBetter/Users/models.py
@@ -12,15 +12,15 @@ class User(AbstractUser):
     email = models.EmailField(_('email address'), unique=True)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    current_classes = models.JSONField()
-    previous_classes = models.JSONField()
+    current_classes = models.JSONField(null=True, blank=True)
+    previous_classes = models.JSONField(null=True, blank=True)
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
     objects= CustomUserManager()
 
     def save(self, *args, **kwargs):
-        self.username = self.university.split('@')[0]
+        self.username = self.email.split('@')[0]
         super().save(*args, **kwargs)
         
     def __str__(self):
