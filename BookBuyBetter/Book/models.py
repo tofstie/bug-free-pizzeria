d569e6ed2c727c95186fb4152bbe6678bed45b5a
unknown <skousboL@ualberta.ca>
2022-01-15 13:51:58 -0700

    Updated Book Table

--- BookBuyBetter/Book/models.py
+++ BookBuyBetter/Book/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Book(AbstractBook):
+    BookID = models.BigAutoField(primary_key=True)
+    Title = models.CharField(max_length=255)
+    Author = models.CharField(max_length=100)
+    ISBN  = models.CharField(max_length=13)
+    Edition = models.IntegerField()
+    OriginalPrice = models.FloatField()
+    AveragePrice = models.FloatField()
