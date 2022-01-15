c61fa2de78f2918b95198c87b859e03a48d0f10a
unknown <skousboL@ualberta.ca>
2022-01-15 16:26:45 -0700

    Adding

--- BookBuyBetter/BookListings/views.py
+++ BookBuyBetter/BookListings/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import BookListing
 
 # Create your views here.
 def index(request):
     return render(request, 'BookListings/index.html')
+
+def bookListingView(request):
+    context = {'potato':'good'}
+    BookListing.objects.get()
+    return render(request, 'BookListings/bookListingView.html', context)
