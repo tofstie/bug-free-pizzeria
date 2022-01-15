3e83b99878d1b7de44bd4a92fa8b459a2ea030ac
tofstie <60015373+tofstie@users.noreply.github.com>
2022-01-15 15:17:13 -0700

    Migrated

--- BookBuyBetter/Book/models.py
+++ BookBuyBetter/Book/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Book(AbstractBook):
+class Book(models.Model):
     BookID = models.BigAutoField(primary_key=True)
     Title = models.CharField(max_length=255)
     Author = models.CharField(max_length=100)
