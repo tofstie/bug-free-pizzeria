e1693c378acc20fb3ef974bb4fe2ec07ef7573a8
tofstie <60015373+tofstie@users.noreply.github.com>
2022-01-15 15:46:01 -0700

    Updated Booklisting user reference

--- BookBuyBetter/BookListings/models.py
+++ BookBuyBetter/BookListings/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 from Books.models import Book
-from Users.models import User
+from django.conf import settings
 
 # Create your models here.
 class BookListing(models.Model):
     ListingID = models.BigAutoField(primary_key=True)
-    Seller =  models.ForeignKey(User, on_delete=models.CASCADE)
+    Seller =  models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     Book =  models.ForeignKey(Book, on_delete=models.CASCADE)
     ListPrice = models.FloatField()
     DateAdded = models.DateTimeField()
