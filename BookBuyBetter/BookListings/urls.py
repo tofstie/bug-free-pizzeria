b9a785767633cc0d88025cc6c3377e29ddb82feb
tofstie <60015373+tofstie@users.noreply.github.com>
2022-01-15 18:00:03 -0700

    Updated base and index

--- BookBuyBetter/BookListings/urls.py
+++ BookBuyBetter/BookListings/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'BookListings'
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('all_listings', views.all_listings, name='all_listings')
 ]
