535c22e385f74ec284b0bc8ea3b2bc8456e63ba1
unknown <skousboL@ualberta.ca>
2022-01-15 22:02:17 -0700

    Add classes built

--- BookBuyBetter/Users/forms.py
+++ BookBuyBetter/Users/forms.py
@@ -1,8 +1,19 @@
 from .models import User
+from ClassCodes.models import ClassCode
 from django.contrib.auth.forms import UserCreationForm# , UserChangeForm <--- If we have time
+from django import forms
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('email','first_name','last_name','location','current_classes','previous_classes')
 
+class AddClassesForm(forms.Form):
+    classes = []
+    for e in ClassCode.objects.all().distinct():
+        classes.append((e.ClassName, e.ClassName))
+
+    Class= forms.ChoiceField( 
+        required = True,
+        widget=forms.Select,
+        choices=classes)
--- BookBuyBetter/Users/views.py
+++ BookBuyBetter/Users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout
-from .forms import CustomUserCreationForm
+from .forms import AddClassesForm, CustomUserCreationForm
 # Create your views here.
 
 
@@ -20,3 +20,15 @@ def register(request):
 
     context = {'form': form}
     return render(request, 'registration/register.html', context)
+
+def addClasses(request):
+    if request.method != "POST":
+        form = AddClassesForm()
+    else:
+        form = AddClassesForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('BookListings:all_listings')
+
+    context = {'form': form}
+    return render(request, 'classes/add_classes.html', context)
