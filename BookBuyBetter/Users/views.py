9a0e4580c0388313ce47e80d6e220e1830377604
unknown <skousboL@ualberta.ca>
2022-01-16 00:16:54 -0700

    Columns

--- BookBuyBetter/Users/views.py
+++ BookBuyBetter/Users/views.py
@@ -1,6 +1,8 @@
+from posixpath import split
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout
 from .forms import AddClassesForm, CustomUserCreationForm
+from .models import User
 # Create your views here.
 
 
@@ -22,13 +24,18 @@ def register(request):
     return render(request, 'registration/register.html', context)
 
 def addClasses(request):
+    classList = []
     if request.method != "POST":
         form = AddClassesForm()
+        classList = request.user.current_classes
+        if classList != None:
+            classList = classList.split(',')
+        
     else:
         form = AddClassesForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('BookListings:all_listings')
 
-    context = {'form': form}
+    context = {'form': form, 'userClasses': classList}
     return render(request, 'classes/add_classes.html', context)
