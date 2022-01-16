18c54b21cbe90abc3828d8dc64633f922f7fe46f
tofstie <60015373+tofstie@users.noreply.github.com>
2022-01-15 21:02:58 -0700

    updated listings + deleted pycache

--- BookBuyBetter/BookListings/views.py
+++ BookBuyBetter/BookListings/views.py
@@ -42,4 +42,5 @@ def new_listing(request):
 def listing(request, listing_id):
     listing_obj = BookListing.objects.get(ListingID=listing_id)
     context = {'listing_obj':listing_obj}
+    print(listing_obj.BookImage.url)
     return render(request, 'BookListings/listing.html', context)
